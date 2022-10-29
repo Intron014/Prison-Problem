8aedbbc022d582961d70d14e0c1a9cd051be61b6
Jorge Benjumea Burillo <me@intron014.com>
2022-10-29 22:07:09 +0200

    IT WORKS!

--- main.c
+++ main.c
@@ -3,40 +3,44 @@
 #include <time.h>
 
 int main() {
-    int pos1, pos2, value, reboots=0;
-    char percent='%';
-    int box[99];
-    int people[99];
+    int pos1, pos2, pos3, value, oldvalue, reboots=0;
+    int box[101];
     srand(time(NULL));
-    reboot:
-    //Asignar números a las cajas
-    for (pos1 = 0; pos1 < 100; pos1++) {
-        value = rand() % 99 + 1;
-        //Evitar números repetidos
-        for (pos2 = 0; pos2 < pos1; pos2++) {
-            if (value == box[pos2]) {
-                value = rand() % 99 + 1;
-                pos2 = 0;
+    for(pos3=0;pos3<=2;pos3++) {
+        reboot:
+        //Asignar números a las cajas
+        box[0] = 0;
+        for (pos1 = 1; pos1 <= 100; pos1++) {
+            value = rand() % 100 + 1;
+            //Evitar números repetidos
+            for (pos2 = 1; pos2 <= pos1; pos2++) {
+                if (value == box[pos2]) {
+                    value = rand() % 100 + 1;
+                    pos2 = 0;
+                }
             }
+            box[pos1] = value;
+            printf("Box - %i: %i\n", pos1, value);
         }
-        box[pos1] = value;
-        printf("Box - %i: %i\n", pos1, value);
-    }
 
-        for (pos1 = 0, pos2=0; pos1 <= 99; pos1++, pos2=0) {
-            value = box[pos1];
-            printf("Value: %i", value);
+        //Comenzamos el sorting
+        for (pos1 = 1, pos2 = 0; pos1 < 100; pos1++, pos2 = 0) {
+            // pos1: Indica la posición en el array (personas); pos2: El número de cajas abiertas*/
+            value = pos1;
+            printf("\n----\nPersona: %i\n----\n", value);
             do {
+                oldvalue = value;
                 value = box[value];
                 pos2++;
-                printf("Box: %i, Value: %i!\n", box, value);
-            } while (value != box[pos1]&&pos2<=50);
-            if(pos2>50){
+                printf("Box: %i - Number: %i\n", oldvalue, value);
+            } while (value != pos1 && pos2 <= 50);
+            printf("Opened Boxes: %i\n", pos2);
+            if (pos2 >= 51) {
                 printf("OOPS...");
                 reboots++;
                 goto reboot;
             }
-            printf("POS: %i, Box: %i, Value: %i!\n", pos1, box, value);
         }
-    printf("Done!\n-----\nReboots: %i", reboots);
+        printf("Done!\n-----\nReboots: %i\n", reboots);
+    }
 }
